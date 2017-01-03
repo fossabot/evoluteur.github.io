791bc8a88d15c96327e6904e4f96b20c27297ee5
Olivier Giulieri <ogiulieri@gmail.com>
2017-01-02 21:59:09 -0800

    New Year, New Web Site.

--- js/me.js
+++ js/me.js
@@ -1,292 +1,471 @@
 
 var mediaList = {
 	evol: [
-		{id:'many-cards', url:'http://evoluteur.github.io/evolutility-ui-jquery/demo/index.html#comics/cards'},
-		{id:'many-list', url:'http://evoluteur.github.io/evolutility-ui-jquery/demo/index.html#comics/list'},
-		{id:'many-bubbles', url:'http://evoluteur.github.io/evolutility-ui-jquery/demo/index.html#comics/bubbles'},
-		{id:'many-charts', url:'http://evoluteur.github.io/evolutility-ui-jquery/demo/index.html#comics/charts'},
-		{id:'one-edit', url:'http://evoluteur.github.io/evolutility-ui-jquery/demo/index.html#comics/list',
-			css: 'hideIfSmall'},
-	],
-	comics: [
 		{
-			id: "incal1",
-			url: "https://smile.amazon.com/Incal-Alejandro-Jodorowsky/dp/1594650934?tag=evolutility-20"
+			id:'many-list', 
+			title: 'List View',
+			url:'http://evoluteur.github.io/evolutility-ui-jquery/demo/index.html#comics/list'
 		},
 		{
-			id: "quete1",
-			url: "https://smile.amazon.com/Quest-Time-Bird-Serge-Tendre/dp/1782763627?tag=evolutility-20"
+			id:'many-bubbles', 
+			title: 'Bubbles View',
+			url:'http://evoluteur.github.io/evolutility-ui-jquery/demo/index.html#comics/bubbles'
+		},
+		{
+			id:'many-charts', 
+			title: 'Charts View',
+			url:'http://evoluteur.github.io/evolutility-ui-jquery/demo/index.html#comics/charts',
+			css: 'hideIfSmall'
+		},
+	],
+	recipes: [
+		{
+			id:'creme-brulee', 
+			title: 'Crèmes brûlées',
+			url:'recipes/desserts/creme-brulee.html'
+		},
+		{
+			id:'pear-tart', 
+			title: 'Pear cream tart',
+			url:'recipes/desserts/pear-cream-tart.html'
+		},
+		{
+			id:'madeleine', 
+			title: 'Madeleines',
+			url:'recipes/desserts/madeleines.html' 
+		},
+	],
+	//comics: randomStart([
+	comics: ([
+		{
+			id: "incal1",
+			title: "L'Incal",
+			url: "https://smile.amazon.com/Incal-Alejandro-Jodorowsky/dp/1594650934?tag=evolutility20-20"
 		},
 		{
 			id: "lama1",
-			url: "http://www.bedetheque.com/serie-115-BD-Lama-blanc.html"
+			title: "Le Lama Blanc",
+			url: "https://smile.amazon.com/White-Lama-Alexandro-Jodorowsky/dp/1594650802?tag=Evolutility20-20"
+		},
+		{
+			id: "orbital1",
+			title: "Orbital",
+			url: "http://www.bedetheque.com/serie-13342-BD-Orbital.html"
+		},
+		{
+			id: "niourk1",
+			title: "Niourk",
+			url: "http://www.bedetheque.com/serie-35353-BD-NiourK.html"
+		},
+		{
+			id: "segments1",
+			title: "Segments",
+			url: "http://www.bedetheque.com/serie-30223-BD-Segments.html"
 		},
 		{
 			id: "saga1",
-			url: "https://smile.amazon.com/Saga-Book-One-Brian-Vaughan/dp/1632150786?tag=evolutility-20"
+			title: "Saga",
+			url: "https://smile.amazon.com/Saga-Book-One-Brian-Vaughan/dp/1632150786?tag=evolutility20-20"
+		},
+		{
+			id: "quete1",
+			title: "La Quête de l'Oiseau du Temps",
+			url: "https://smile.amazon.com/Quest-Time-Bird-Serge-Tendre/dp/1782763627?tag=evolutility20-20"
 		},
 		{
 			id: "monstre1",
+			title: "Le Sommeil du Monstre",
 			url: "http://www.bedetheque.com/BD-Sommeil-du-monstre-Tome-1-1348.html"
 		},
 		{
 			id: "pemaling1",
+			title: "Pema Ling",
 			url: "http://www.bedetheque.com/serie-11520-BD-Pema-Ling.html"
 		},
 		{
 			id: "aslak1",
+			title: "Aslak",
 			url: "http://www.bedetheque.com/serie-27075-BD-Aslak.html"
 		},
 		{
 			id: "4pouvoir1",
+			title: "Le Quatrieme Pouvoir",
 			url: "http://www.bedetheque.com/serie-592-BD-Quatrieme-pouvoir.html"
 		},
 		{
 			id: "acriborea",
+			title: "Acriborea",
 			url: "http://www.bedetheque.com/serie-13233-BD-Acriborea.html"
 		},
 		{
 			id: "ivressefantom1",
+			title: "L'Ivresse des Fantomes",
 			url: "http://www.bedetheque.com/serie-15118-BD-Ivresse-des-fantomes.html"
 		},
 		{
-			id: "niourk1",
-			url: "http://www.bedetheque.com/serie-35353-BD-NiourK.html"
-		},
-		{
 			id: "ronin",
-			url: "https://smile.amazon.com/Frank-Millers-Ronin-Miller/dp/0930289218?tag=evolutility-20"
+			title: "Ronin",
+			url: "https://smile.amazon.com/Frank-Millers-Ronin-Miller/dp/0930289218?tag=evolutility20-20"
 		},
 		{
 			id: "carmenmc1",
+			title: "Carmen Mc-Callum",
 			url: "http://www.bedetheque.com/serie-70-BD-Carmen-Mc-Callum.html"
 		},
 		{
 			id: "aquablue1",
+			title: "Aquablue",
 			url: "http://www.bedetheque.com/serie-48-BD-Aquablue.html"
 		},
 		{
 			id: "zarkass1",
+			title: "Piege sur Zarkass",
 			url: "http://www.bedetheque.com/serie-36598-BD-Piege-sur-Zarkass.html"
 		},
 		{
 			id: "low",
+			title: "Low",
 			url: "https://smile.amazon.com/Low-Vol-1-Delirium-Hope/dp/1632151944"
 		},
 		{
 			id: "nirvana1",
+			title: "Nirvana",
 			url: "http://www.bedetheque.com/serie-28647-BD-Nirvana.html"
 		},
 		{
 			id: "ocean-amour",
+			title: "Un Ocean d\'Amour",
 			url: "http://www.bedetheque.com/serie-44836-BD-Un-ocean-d-amour.html"
 		},
 		{
 			id: "chininkel1",
+			title: "Le Grand Pouvoir du Chninkel",
 			url: "http://www.bedetheque.com/serie-26-BD-Grand-pouvoir-du-Chninkel.html"
 		},
 		{
 			id: "legendesoublie1",
+			title: "Legendes des Contrees Oubliees",
 			url: "http://www.bedetheque.com/serie-2-BD-Legendes-des-contrees-oubliees.html"
 		},
 		{
 			id: "kevin1",
+			title: "Litteul Kevin",
 			url: "http://www.bedetheque.com/serie-560-BD-Litteul-Kevin.html"
 		},
 		{
 			id: "sillage1",
+			title: "Sillage",
 			url: "http://www.bedetheque.com/serie-18-BD-Sillage.html"
 		},
 		{
 			id: "edena1",
-			url: "https://smile.amazon.com/Moebius-Library-World-Edena/dp/1506702163?tag=evolutility-20"
+			title: "Edena",
+			url: "https://smile.amazon.com/Moebius-Library-World-Edena/dp/1506702163?tag=evolutility20-20"
 		},
 		{
 			id: "okko1",
+			title: "Okko",
 			url: "http://www.bedetheque.com/serie-10529-BD-Okko.html"
 		},
 		{
 			id: "thorgal1",
+			title: "Thorgal",
 			url: "http://www.bedetheque.com/serie-12-BD-Thorgal.html"
 		},
 		{
 			id: "chimere1",
+			title: "Chimeres 1887",
 			url: "http://www.bedetheque.com/serie-29524-BD-Chimeres-1887.html"
 		},
 		{
 			id: "fleaudieux1",
+			title: "Le Fleau des Dieux",
 			url: "http://www.bedetheque.com/serie-526-BD-Fleau-des-dieux.html"
 		},
 		{
 			id: "lanfeust1",
+			title: "Lanfeust de Troy",
 			url: "http://www.bedetheque.com/serie-6-BD-Lanfeust-de-Troy.html"
 		},
 		{
 			id: "oms",
+			title: "OMS en serie",
 			url: "http://www.bedetheque.com/serie-35557-BD-OmS-en-serie.html"
 		},
 		{
 			id: "urban1",
+			title: "Urban",
 			url: "http://www.bedetheque.com/serie-29156-BD-Urban.html"
 		},
 		{
 			id: "largo1",
+			title: "Largo Winch",
 			url: "http://www.bedetheque.com/serie-14-BD-Largo-Winch.html"
 		},
 		{
 			id: "skydoll1",
-			url: "https://www.amazon.com/Sky-Doll-Vol-Barbara-Canepa/dp/0785132368"
+			title: "Sky Doll",
+			url: "https://smile.amazon.com/Sky-Doll-Vol-Barbara-Canepa/dp/0785132368/?tag=Evolutility20-20"
 		},
 		{
 			id: "vampire-benares1",
+			title: "Le Vampire de Benares",
 			url: "http://www.bedetheque.com/serie-28231-BD-Vampire-de-Benares.html"
 		},
 		{
 			id: "neffous1",
+			title: "La Nef des Fous",
 			url: "http://www.bedetheque.com/serie-36-BD-Nef-des-fous.html"
 		},
 		{
-			id: "segments1",
-			url: "http://www.bedetheque.com/serie-30223-BD-Segments.html"
-		},
-		{
-			id: "orbital1",
-			url: "http://www.bedetheque.com/serie-13342-BD-Orbital.html"
-		},
-		{
 			id: "uw11",
+			title: "Universal War One",
 			url: "http://www.bedetheque.com/serie-279-BD-Universal-War-One.html"
 		},
 		{
 			id: "goldencity1",
+			title: "Golden City",
 			url: "http://www.bedetheque.com/serie-289-BD-Golden-City.html"
 		},
 		{
 			id: "manoir1",
+			title: "Le Manoir des Murmures",
 			url: "http://www.bedetheque.com/serie-16625-BD-Manoir-des-murmures.html"
 		},
 		{
 			id: "pandamonia1",
+			title: "Pandamonia",
 			url: "http://www.bedetheque.com/serie-27333-BD-Pandamonia.html"
 		},
 		{
 			id: "travis1",
+			title: "Travis",
 			url: "http://www.bedetheque.com/serie-75-BD-Travis.html"
 		},
 		{
 			id: "grandmort1",
+			title: "Le Grand Mort",
 			url: "http://www.bedetheque.com/serie-16623-BD-Grand-Mort.html"
 		},
 		{
 			id: "metabaron1",
+			title: "Les Meta-Barons",
 			url: "http://www.bedetheque.com/serie-5124-BD-Meta-barons.html"
 		},
 		{
 			id: "technoperes1",
+			title: "Les Technopères",
 			url: "http://www.bedetheque.com/serie-65-BD-Technoperes.html"
 		},
 		{
 			id: "passagersvent1",
+			title: "Les Passagers du Vent",
 			url: "http://www.bedetheque.com/serie-108-BD-Passagers-du-vent.html"
 		},
 		{
 			id: "garulfo1",
+			title: "Garulfo",
 			url: "http://www.bedetheque.com/serie-57-BD-Garulfo.html"
 		},
 		{
 			id: "meteors",
+			title: 'Meteors',
 			url: "http://www.bedetheque.com/serie-17452-BD-Meteors.html"
 		},
 		{
 			id: "solo",
+			title: 'Solo',
 			url: "http://www.bedetheque.com/serie-44145-BD-Solo.html"
 		}
-	],
+	]),
 	'comics-amazon': [
 		{
-			id: "incal1",
-			url: "https://smile.amazon.com/Incal-Alejandro-Jodorowsky/dp/1594650934?tag=evolutility-20"
+			id: "incal",
+			title: 'The Incal',
+			authors: '',
+			description: '',
+			url: "https://smile.amazon.com/Incal-Alejandro-Jodorowsky/dp/1594650934/?tag=evolutility20-20"
+		},
+		{
+			id: "lama",
+			title: 'The White Lama',
+			url: "https://smile.amazon.com/White-Lama-Alexandro-Jodorowsky/dp/1594650802/?tag=Evolutility20-20"
 		},
 		{
 			id: "edena",
-			url: "https://smile.amazon.com/Moebius-Library-World-Edena/dp/1506702163?tag=evolutility-20"
+			title: 'Edena',
+			authors: '',
+			url: "https://smile.amazon.com/Moebius-Library-World-Edena/dp/1506702163/?tag=evolutility20-20"
+		},
+		{
+			id: "nikopol",
+			title: 'The Nikopol Trilogy',
+			authors: '',
+			url: "https://smile.amazon.com/Nikopol-Trilogy-Enki-Bilal/dp/1782763538/?tag=Evolutility20-20"
 		},
 		{
 			id: "quest-time-bird",
-			url: "https://smile.amazon.com/Quest-Time-Bird-Serge-Tendre/dp/1782763627?tag=evolutility-20"
+			title: 'The Quest for the Time Bird',
+			authors: '',
+			url: "https://smile.amazon.com/Quest-Time-Bird-Serge-Tendre/dp/1782763627/?tag=evolutility20-20"
+		}
+	],
+	'comics-us': [
+		{
+			id: "saga1",
+			title: "Saga",
+			url: "https://smile.amazon.com/Saga-Book-One-Brian-Vaughan/dp/1632150786?tag=evolutility20-20"
+		},
+		{
+			id: "low",
+			title: "Low",
+			url: "https://smile.amazon.com/Low-Vol-1-Delirium-Hope/dp/1632151944?tag=evolutility20-20"
+		},
+		{
+			id: "ronin",
+			title: "Ronin",
+			url: "https://smile.amazon.com/Frank-Millers-Ronin-Miller/dp/0930289218?tag=evolutility20-20"
 		},
 	],
 	fractals: [
-		{id:'mandelbrot-1', url:'https://en.wikipedia.org/wiki/Benoit_Mandelbrot#/media/File:Mandel_zoom_08_satellite_antenna.jpg'},
-		{id:'mandelbrot-2', url:'https://en.wikipedia.org/wiki/Benoit_Mandelbrot#/media/File:Newton-lplane-Mandelbrot.jpg'},
-		{id:'mandelbrot-3', url:'https://en.wikipedia.org/wiki/Mandelbrot_set'},
-		{id:'snowflake', url:'https://en.wikipedia.org/wiki/Fractal'},
+		{id:'mandelbrot-1', title:'Mandelbrot set', url:'https://en.wikipedia.org/wiki/Benoit_Mandelbrot#/media/File:Mandel_zoom_08_satellite_antenna.jpg'},
+		{id:'mandelbrot-2', title:'Mandelbrot set', url:'https://en.wikipedia.org/wiki/Benoit_Mandelbrot#/media/File:Newton-lplane-Mandelbrot.jpg'},
+		{id:'mandelbrot_one_island', title:'Julia "island"', url:'https://en.wikipedia.org/wiki/Mandelbrot_set#/media/File:Mandel_zoom_15_one_island.jpg'},
+		{id:'snowflake', title:'Snowflake', url:'http://gizmodo.com/this-physicists-designer-snowflakes-are-dazzling-1748893157'},
 	],
 	chakras: [
-		{id:'ch7-silver2', url:'http://www.chakradesign.com'},
-		{id:'cc7-smcjalc', url:'http://www.chakradesign.com/gallery.html'},
-		{id:'cc7-gold', url:'http://www.chakradesign.com/gallery.html'},
-		{id:'cc7-arwen', url:'http://www.chakradesign.com/gallery.html'},
+		{	
+			id:'ch7-silver2', 
+			title: 'Amethyst, Sodalite, Angelite, Amazonite, Amber, Carnelian, Red jasper',
+			url:'http://www.chakradesign.com'
+		},
+		{
+			id:'cc7-smcjalc',
+			title: 'Clear Calcite, Lapis Lazuli, Angelite, Green Jade, Citrine, Peach Moonstone, Smoky Quartz',
+			url:'http://www.chakradesign.com/gallery.html'
+		},
+		{
+			id:'cc7-gold',
+			title: 'Aragonite, Denim Lapis, Angelite, Unakite, Dark Amber, Moonstone, Red Tiger Eye',
+			url:'http://www.chakradesign.com/gallery.html'
+		},
+		{
+			id:'cc7-arwen',
+			title: 'Labradorite, Pietersite, Aquamarine, Chrysocolla, Amber, Peach Moonstone, Red Tiger Eye',
+			url:'http://www.chakradesign.com/gallery.html'
+		},
 	],
-	movies: [
-		{id:'bladerunner', url:'http://www.imdb.com/title/tt0083658/'},
-		{id:'matrix', url:'http://www.imdb.com/title/tt0133093/'},
-		{id:'gattaca', url:'http://www.imdb.com/title/tt0119177/'},
-		{id:'fifthelem', url:'http://www.imdb.com/title/tt0119116/'},
-		{id:'dark-city', url:'http://www.imdb.com/title/tt0118929/'},
-		{id:'avatar', url:'http://www.imdb.com/title/tt0499549/'},
-		{id:'inception', url:'http://www.imdb.com/title/tt1375666/'},
-		{id:'star-wars', url:'http://www.imdb.com/title/tt1185834/'},
-		{id:'stargate', url:'http://www.imdb.com/title/tt0111282/'},
-		{id:'ghost-in-shell', url:'http://www.imdb.com/title/tt0113568/'},
-		{id:'enders', url:'http://www.imdb.com/title/tt1731141/'},
-		{id:'xmen', url:'http://www.imdb.com/title/tt0120903/'},
-		{id:'guardian-galaxy', url:'http://www.imdb.com/title/tt2015381/'},
-		{id:'johncarter', url:'http://www.imdb.com/title/tt0401729/'} 
-	]
+	movies: randomStart([
+		{id:'matrix', title:'Matrix', url:'http://www.imdb.com/title/tt0133093/'},
+		{id:'dark-city', title:'Dark City', url:'http://www.imdb.com/title/tt0118929/'},
+		{id:'gattaca', title:'Gattaca', url:'http://www.imdb.com/title/tt0119177/'},
+		{id:'bladerunner', title:'Blade Runner', url:'http://www.imdb.com/title/tt0083658/'},
+		{id:'vendetta', title:'V for Vendetta', url:'http://www.imdb.com/title/tt0434409/'},
+		{id:'fifthelem', title:'The Fifth Element', url:'http://www.imdb.com/title/tt0119116/'},
+		{id:'star-wars', title:'Star Wars', url:'http://www.imdb.com/title/tt1185834/'},
+		{id:'avatar', title:'Avatar', url:'http://www.imdb.com/title/tt0499549/'},
+		{id:'dune', title:'Dune', url:'http://www.imdb.com/title/tt0087182/'},
+		{id:'man', title:'The Man from Earth', url:'http://www.imdb.com/title/tt0756683'},
+		{id:'kpax', title:'K-PAX', url:'http://www.imdb.com/title/tt0272152/'},
+		{id:'inception', title:'Inception', url:'http://www.imdb.com/title/tt1375666/'},
+		{id:'stargate', title:'Star Gate', url:'http://www.imdb.com/title/tt0111282/'},
+		{id:'enders', title:'Ender\'s Game', url:'http://www.imdb.com/title/tt1731141/'},
+		{id:'guardian-galaxy', title:'Guardian of the Galaxy', url:'http://www.imdb.com/title/tt2015381/'},
+		{id:'xmen', title:'X-Men', url:'http://www.imdb.com/title/tt0120903/'},
+		{id:'johncarter', title:'John Carter', url:'http://www.imdb.com/title/tt0401729/'}, 
+		{id:'ghost-in-shell', title:'Ghost in the Shell', url:'http://www.imdb.com/title/tt0113568/'},
+		{id:'akira', title:'Akira', url:'http://www.imdb.com/title/tt0094625/'},
+	]),
+	neige: [1,2,3,4].map(function(i){
+		return {
+			id: 'neige-me-'+i, 
+			title:'Attempt '+i, 
+			url:'http://www.imdb.com/title/tt0094625/'
+		}
+	})
 }
-var extensions={
-	evol: '.gif',
-	comics: '.jpeg', 
-	fractals: '.jpeg', 
-	chakras: '.jpg', 
-	movies: '.jpg',
+
+function randomStart(arr){
+	var ln = arr.length,
+		rnd = Math.floor(Math.random() * (ln));
+
+	return arr.slice(rnd, ln).concat(arr.slice(0, rnd))
 }
-function moreIdx(id){
-	if(id==='movies'){
-		return 5;
-	}else if(id==='comics'){
-		return 6;
-	}
-	return 10;
+
+function showIceCream(){
+	document.getElementById('IceCream').style.display='block';
+	document.getElementById('linkIceCream').remove()
 }
+
 function more(id, min){
-	document.getElementById(id).innerHTML=mosaic(id, true)
+	document.getElementById(id+'_x').innerHTML=mosaic(id, true)
+}
+
+var linkCaptions={
+	comics: 'More graphic novels...',
+	'comics-2': 'Even more graphic novels...',
+	movies: 'More sci-fi movies...'
+}
+function pixDir(id){
+	if(id.startsWith('comic')){
+		return 'pix/comics/'
+	}else if(id==='neige'){
+		return 'pix/neige/'
+	}else{
+		return 'pix/'+id+'/'
+	}
 }
 
 function mosaic(id, more){
-	var preview=moreIdx(id),
-	 	ext=extensions[id],
+	if(id==='comics-2'){
+		id='comics';
+		more=2;
+	}else{
+		more= more ? 1 : 0;
+	}
+	var preview=(id==='movies' || id==='comics')?6:10,
+	 	ext=id==='evol'?'.gif':'.jpg',
 		arrList=mediaList[id],
-		arrListP = more ? arrList.slice(preview, arrList.length) : arrList.slice(0, preview),
+		ml = more ? 24 : arrList.length,
+		arrListP = more ? arrList.slice(preview, ml) : arrList.slice(0, preview),
+		linkTarget = id!='recipes',
+		pixPath = pixDir(id),
 		imageLink = function (m){
-			if(m.url){
-				return '<a href="'+m.url+'" target="_new"'+(m.css?(' class="'+m.css+'"'):'')+'><img src="pix/'+id+'/'+m.id+ext+'"/></a>';
-			}else{
-				return '<img src="pix/'+id+'/'+m.id+ext+'"/>';
+			var pix='<img src="'+pixPath+m.id+ext+'"/>';
+			if(m.title){
+				pix += '<div class="pixTitle">'+m.title+'</div>';
 			}
+			var h = '<div class="pixTitleHolder">';
+			if(m.url){
+				h+= '<a href="'+m.url+'"'+
+						(linkTarget?' target="_blank"':'')+
+						(m.css?(' class="'+m.css+'"'):'')+
+					'>'+pix+'</a>';
+			}else{
+				h+= pix;
+			}
+			h+='</div>';
+			return h;
+		},
+		mm;
+
+		if(more===2){
+			arrListP = arrList.slice(ml, arrList.length);
+		}else{
+			arrListP = more ? arrList.slice(preview, ml) : arrList.slice(0, preview);
 		}
-		mm = arrListP.map(imageLink)
+		mm = arrListP.map(imageLink);
+
 	if(arrList.length>preview & !more){
-		mm.push('<span id="'+id+'"><a class="linkMore" href="javascript:more(\''+id+'\','+preview+')">more of my favorite '+id+'...</a></span>');
+		mm.push('<span id="'+id+'_x"><a class="linkMore" href="javascript:more(\''+id+'\','+preview+')">['+linkCaptions[id]+']</a></span>');
 	}
-	if(more && id==='comics'){
-		mm.push('<p>Today, you could treat yourself with some excellent French graphic novels like '+
-			'<a href="https://smile.amazon.com/Incal-Alejandro-Jodorowsky/dp/1594650934?tag=evolutility-20">L\'Incal</a>, '+
-			'<a href="https://smile.amazon.com/Moebius-Library-World-Edena/dp/1506702163?tag=evolutility-20">Edena</a>, '+
-			'or <a href="https://smile.amazon.com/Quest-Time-Bird-Serge-Tendre/dp/1782763627?tag=evolutility-20">La Quête de l\'Oiseau du Temps</a> '+
-			' available in English translation at Amazon.com.</p>');
+	if(more===1 && (id==='comics')){
+		mm.push('<span id="'+id+'-2_x"><a class="linkMore" href="javascript:more(\'comics-2\')">[Even more graphic novels...]</a></span>');
+		mm.push('<p class="clearer">Did you know that some of the best French graphic novels are now available in English?</p>');
 		mm.push(mediaList['comics-amazon'].map(imageLink).join(''))
+		mm.push('<p class="clearer">I\'m also getting into American comics.</p>');
+		mm.push(mediaList['comics-us'].map(imageLink).join(''))
 	}
+	mm.push('<div class="clearer"/>')
 	return mm.join('');
 }
